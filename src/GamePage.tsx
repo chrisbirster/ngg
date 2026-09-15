943812b655ad5c685a813209be57f340e8a89ca2
Chris Birster <christopher.birster@gmail.com>
2026-09-14 22:20:14 -0400

    feat: match XO Arena game page mockup (#4)

--- src/GamePage.tsx
+++ src/GamePage.tsx
@@ -1,5 +1,5 @@
 import * as stylex from "@stylexjs/stylex";
-import { GamePlayer, PromoBanner, Sidebar, SocialPanel } from "./components";
+import { GameHeading, GamePlayer, PromoBanner, Sidebar, SocialPanel } from "./components";
 import { xoArena } from "./catalog";
 import { s } from "./styles.stylex";
 
@@ -7,13 +7,10 @@ export default function GamePage() {
   const game = xoArena;
   return <main {...stylex.attrs(s.container)}>
     <PromoBanner />
-    <div {...stylex.attrs(s.titleRow)}>
-      <div><div {...stylex.attrs(s.eyebrow)}>Featured game · by <a href={game.creator.url}>{game.creator.name}</a></div><h1 {...stylex.attrs(s.title)}>{game.title}</h1><div {...stylex.attrs(s.subtitle)}>{game.genres.join(" · ")} · Multiplayer · HTML5 · Free</div></div>
-      <span {...stylex.attrs(s.livePill)}>● PLAYABLE NOW</span>
-    </div>
     <div {...stylex.attrs(s.layout)}>
-      <div id="player"><GamePlayer /><SocialPanel slug={game.slug}/></div>
+      <section id="player" {...stylex.attrs(s.gameCard)}><GameHeading game={game} /><GamePlayer /></section>
       <Sidebar game={game}/>
+      <SocialPanel slug={game.slug}/>
     </div>
     <footer {...stylex.attrs(s.footer)}><span>© 2026 NGG.GG · Built for creators.</span><span>Games · Community · Privacy · Terms</span></footer>
   </main>;
